2f67ee49c77f1794b203f0f9bfdfcb452196322d
appsius <ilyas.ustunkaya234434@gmail.com>
2020-03-28 23:27:00 +0300

    Wrong letters and Play again

--- script.js
+++ script.js
@@ -1,17 +1,230 @@
 const wordEl = document.getElementById('word');
 const wrongLettersEl = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-again');
+const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
 
-const figureParts = document.querySelectorAll('.figure-parts');
+const figureParts = document.querySelectorAll('.figure-part');
 
-const words = ['application', 'programming', 'interface', 'wizard'];
+const words = [
+	'abruptly',
+	'absurd',
+	'abyss',
+	'affix',
+	'askew',
+	'avenue',
+	'awkward',
+	'axiom',
+	'azure',
+	'bagpipes',
+	'bandwagon',
+	'banjo',
+	'bayou',
+	'beekeeper',
+	'bikini',
+	'blitz',
+	'blizzard',
+	'boggle',
+	'bookworm',
+	'boxcar',
+	'boxful',
+	'buckaroo',
+	'buffalo',
+	'buffoon',
+	'buxom',
+	'buzzard',
+	'buzzing',
+	'buzzwords',
+	'caliph',
+	'cobweb',
+	'cockiness',
+	'croquet',
+	'crypt',
+	'curacao',
+	'cycle',
+	'daiquiri',
+	'dirndl',
+	'disavow',
+	'dizzying',
+	'duplex',
+	'dwarves',
+	'embezzle',
+	'equip',
+	'espionage',
+	'euouae',
+	'exodus',
+	'faking',
+	'fishhook',
+	'fixable',
+	'fjord',
+	'flapjack',
+	'flopping',
+	'fluffiness',
+	'flyby',
+	'foxglove',
+	'frazzled',
+	'frizzled',
+	'fuchsia',
+	'funny',
+	'gabby',
+	'galaxy',
+	'galvanize',
+	'gazebo',
+	'giaour',
+	'gizmo',
+	'glowworm',
+	'glyph',
+	'gnarly',
+	'gnostic',
+	'gossip',
+	'grogginess',
+	'haiku',
+	'haphazard',
+	'hyphen',
+	'iatrogenic',
+	'icebox',
+	'injury',
+	'ivory',
+	'ivy',
+	'jackpot',
+	'jaundice',
+	'jawbreaker',
+	'jaywalk',
+	'jazziest',
+	'jazzy',
+	'jelly',
+	'jigsaw',
+	'jinx',
+	'jiujitsu',
+	'jockey',
+	'jogging',
+	'joking',
+	'joyful',
+	'juicy',
+	'jukebox',
+	'jumbo',
+	'jovial',
+	'kayak',
+	'kazoo',
+	'keyhole',
+	'khaki',
+	'kilobyte',
+	'kiosk',
+	'kitsch',
+	'kiwifruit',
+	'klutz',
+	'knapsack',
+	'larynx',
+	'lengths',
+	'lucky',
+	'luxury',
+	'lymph',
+	'marquis',
+	'matrix',
+	'megahertz',
+	'microwave',
+	'mnemonic',
+	'mystify',
+	'naphtha',
+	'nightclub',
+	'nowadays',
+	'numbskull',
+	'nymph',
+	'onyx',
+	'ovary',
+	'oxidize',
+	'oxygen',
+	'pajama',
+	'peekaboo',
+	'phlegm',
+	'pixel',
+	'pizazz',
+	'pneumonia',
+	'polka',
+	'pshaw',
+	'psyche',
+	'puppy',
+	'puzzling',
+	'quartz',
+	'queue',
+	'quips',
+	'quixotic',
+	'quiz',
+	'quizzes',
+	'quorum',
+	'razzmatazz',
+	'rhubarb',
+	'rhythm',
+	'rickshaw',
+	'schnapps',
+	'scratch',
+	'shiv',
+	'snazzy',
+	'sphinx',
+	'spritz',
+	'squawk',
+	'staff',
+	'strength',
+	'strengths',
+	'stretch',
+	'stronghold',
+	'stymied',
+	'subway',
+	'swivel',
+	'syndrome',
+	'thriftless',
+	'thumbscrew',
+	'topaz',
+	'transcript',
+	'transgress',
+	'transplant',
+	'triphthong',
+	'twelfth',
+	'twelfths',
+	'unknown',
+	'unworthy',
+	'unzip',
+	'uptown',
+	'vaporize',
+	'vixen',
+	'vodka',
+	'voodoo',
+	'vortex',
+	'voyeurism',
+	'walkway',
+	'waltz',
+	'wave',
+	'wavy',
+	'waxy',
+	'wellspring',
+	'wheezy',
+	'whiskey',
+	'whizzing',
+	'whomever',
+	'wimpy',
+	'witchcraft',
+	'wizard',
+	'woozy',
+	'wristwatch',
+	'wyvern',
+	'xylophone',
+	'yachtsman',
+	'yippee',
+	'yoked',
+	'youthful',
+	'yummy',
+	'zephyr',
+	'zigzag',
+	'zigzagging',
+	'zilch',
+	'zipper',
+	'zodiac'
+];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-const correctLetters = ['w', 'i', 'z', 'a', 'r', 'd'];
+const correctLetters = [];
 const wrongLetters = [];
 
 // Show hidden word
@@ -39,7 +252,28 @@ function displayWord() {
 
 // Update the wrong letters
 function updateWrongLettersEl() {
-	console.log('Update wrong');
+	// Display wrong letters
+	wrongLettersEl.innerHTML = `
+			${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+			${wrongLetters.map(letter => `<span>${letter}</span>`)}
+		`;
+
+	// Display parts
+	figureParts.forEach((part, index) => {
+		const errors = wrongLetters.length;
+
+		if (index < errors) {
+			part.style.display = 'block';
+		} else {
+			part.style.display = 'none';
+		}
+	});
+
+	// Check if lost
+	if (wrongLetters.length === figureParts.length) {
+		finalMessage.innerText = 'Unfortunately you lost. 😥';
+		popup.style.display = 'flex';
+	}
 }
 
 // Show Notification
@@ -75,6 +309,21 @@ window.addEventListener('keydown', e => {
 			}
 		}
 	}
+});
+
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+	// Empty arrays
+	correctLetters.splice(0);
+	wrongLetters.splice(0);
+
+	selectedWord = words[Math.floor(Math.random() * words.length)];
+
+	displayWord();
+
+	updateWrongLettersEl();
+
+	popup.style.display = 'none';
 });
 
 displayWord();
